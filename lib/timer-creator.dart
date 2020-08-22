e0855d8558b4427a971871615896c7452751d6db
FlorianFeka <florianfeka05@gmail.com>
2020-08-22 21:09:54 +0200

    add time validation
    resolves #2

--- lib/timer-creator.dart
+++ lib/timer-creator.dart
@@ -7,6 +7,29 @@ class TimerCreatorWidget extends StatelessWidget {
 
   TimerCreatorWidget(this.addTimerFunction);
 
+  void showWrongTimeValidation(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: new Text("Timer time"),
+            content: Text("Please enter a time greather than 0 seconds!"),
+            actions: <Widget>[
+              Row(
+                children: <Widget>[
+                  FlatButton(
+                    child: new Text("OK"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
@@ -15,6 +38,10 @@ class TimerCreatorWidget extends StatelessWidget {
         DatePicker.showTimePicker(context,
             showTitleActions: true,
             currentTime: DateTime(2000), onConfirm: (time) {
+          if (time.hour == 0 && time.minute == 0 && time.second == 0) {
+            this.showWrongTimeValidation(context);
+            return;
+          }
           this.addTimerFunction(time);
         });
       },
